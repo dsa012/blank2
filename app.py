e2a4cd37ec716a325415c4d75de1716a863ce3cd
dsa12 <140321591+dsa012@users.noreply.github.com>
2023-11-25 19:16:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,44 @@
-import streamlit as st
-import psutil
+from llama_index import (
+    SimpleDirectoryReader,
+    VectorStoreIndex,
+    ServiceContext,
+    download_loader
+)
+from llama_index.llms import LlamaCPP
+from llama_index.llms.llama_utils import (
+    messages_to_prompt,
+    completion_to_prompt,
+)
+from llama_index.embeddings import HuggingFaceEmbedding
 
-# Get RAM and disk storage information using psutil
-ram_info = psutil.virtual_memory()
-disk_info = psutil.disk_usage('/')
 
-# Display RAM information
-st.write(f"Total RAM: {ram_info.total / (1024 ** 3):.2f} GB")
-st.write(f"Available RAM: {ram_info.available / (1024 ** 3):.2f} GB")
+model_url = "https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main/llama-2-13b-chat.Q5_K_M.gguf"
+llm = LlamaCPP(
+    model_url=model_url,
+    model_path=None,
+    temperature=0.1,
+    max_new_tokens=256,
+    context_window=3900,
+    generate_kwargs={},
+    model_kwargs={},
+    messages_to_prompt=messages_to_prompt,
+    completion_to_prompt=completion_to_prompt,
+    verbose=True,
+)
 
-# Display disk storage information
-st.write(f"Total Disk Space: {disk_info.total / (1024 ** 3):.2f} GB")
-st.write(f"Available Disk Space: {disk_info.free / (1024 ** 3):.2f} GB")
-import streamlit as st
-import psutil
+embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+service_context = ServiceContext.from_defaults(
+    llm=llm,
+    embed_model=embed_model,
+)
 
-# Get RAM and disk storage information using psutil
-ram_info = psutil.virtual_memory()
-disk_info = psutil.disk_usage('/')
+ZendeskReader = download_loader("ZendeskReader")
+loader = ZendeskReader(zendesk_subdomain="stackuphelpcentre", locale="en-us")
+documents = loader.load_data()
 
-# Display RAM information
-st.write(f"Total RAM: {ram_info.total / (1024 ** 3):.2f} GB")
-st.write(f"Available RAM: {ram_info.available / (1024 ** 3):.2f} GB")
-
-# Display disk storage information
-st.write(f"Total Disk Space: {disk_info.total / (1024 ** 3):.2f} GB")
-st.write(f"Available Disk Space: {disk_info.free / (1024 ** 3):.2f} GB")
+index = VectorStoreIndex.from_documents(
+    documents, service_context=service_context
+)
+query_engine = index.as_query_engine()
+response = query_engine.query("What is Learn & Earn v2.0?")
+print(response)
